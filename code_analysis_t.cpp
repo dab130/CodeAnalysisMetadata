6e1ad03cdde5cd9de2c0a597e57da1a159058b1c
dab130 <dab130@zips.uakron.edu>
2018-04-06 10:52:40 -0400

    added test case for when the input is from standard input and a language cannot be determined,

--- code_analysis_t.cpp
+++ code_analysis_t.cpp
@@ -190,6 +190,24 @@ int main() {
         assert(code_analysis(request) == false);
 		
 	}
+	{
+		//Test case: When the input is from standard input and a language cannot be determined, 
+		//output the error message “Using stdin requires a declared language”	
+        analysis_request request;
+        request.given_filename  = "";
+        request.entry_filename  = "";
+        request.given_url       = "";
+        request.option_filename = "test.cpsdf";
+        request.option_url      = "";
+        request.option_language = "";
+		
+        auto filename = analysis_filename(request);
+        assert(filename == "test.cpsdf");
+        assert(analysis_url(request) == "");
+        assert(analysis_language(request, filename) == "");
+        assert(code_analysis(request) == false);
+		
+	}
 
     return 0;
 }
